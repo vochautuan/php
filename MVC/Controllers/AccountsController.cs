5339df962142d8a0ed9ad8b26cd4f2abebeb6ba8
châu tuấn <0306191481@caothang.edu.vn>
2021-10-08 02:29:33 +0700

    first commit

---
+++ MVC/Controllers/AccountsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVC.Models;
+namespace MVC.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Models
+{
+    public class Account
+    {
+        public string Productname { get; set; }
+        public string Information { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+
+
+    }
+}
